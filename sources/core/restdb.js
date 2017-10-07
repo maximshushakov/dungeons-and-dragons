5577890613607dd7b39deabbf93be80bdaa6b911
ShushakovM <shushakov.maxim@gmail.com>
2017-10-07 14:16:33 +0700

    Add helper for rest db

---
+++ sources/core/restdb.js
@@ -0,0 +1,14 @@
+import Helper from 'core/helper';
+
+class RestDB {
+
+	static get(type) {
+		return Helper.ajax({ url: `https://cards-5d46.restdb.io/rest/${type}`, type: 'GET',  key: '58d3ba9881f530cf439b3079' });
+	}
+
+	static post(type, data) {
+		return Helper.ajax({ url: `https://cards-5d46.restdb.io/rest/${type}`, type: 'POST',  key: '5948a6725f54052560916824', data });
+	}
+}
+
+export default RestDB;
